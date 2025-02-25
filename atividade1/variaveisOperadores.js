c54f74cd43cadafe589cec7687d5a1c6a4818c4f
Pablo Dias <90719668+pablodixs@users.noreply.github.com>
2025-02-25 10:50:45 -0300

    Add files via upload

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,12 @@
+let var1 = 10
+let var2 = 7
+
+let resultadoSoma = var1 + var2
+let resultadoSubtracao = var1 - var2
+let resultadoMultiplicacao = var1 * var2
+let resultadoDivisao = var1 / var2
+
+console.log(`Resultado: ${resultadoSoma} entre ${var1} e ${var2}`)
+console.log(`Resultado: ${resultadoSubtracao} entre ${var1} e ${var2}`)
+console.log(`Resultado: ${resultadoMultiplicacao} entre ${var1} e ${var2}`)
+console.log(`Resultado: ${resultadoDivisao} entre ${var1} e ${var2}`)
