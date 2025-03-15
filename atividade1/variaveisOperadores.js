19ab7f0bed69455497fa29292f2e9dc39f2e94fb
Pablo <pabllosoarez@gmail.com>
2025-03-15 16:11:40 -0300

    Improve console log messages for arithmetic operations

--- atividade1/variaveisOperadores.js
+++ atividade1/variaveisOperadores.js
@@ -6,7 +6,13 @@ let resultadoSubtracao = var1 - var2
 let resultadoMultiplicacao = var1 * var2
 let resultadoDivisao = var1 / var2
 
-console.log(`Resultado: ${resultadoSoma} entre ${var1} e ${var2}`)
-console.log(`Resultado: ${resultadoSubtracao} entre ${var1} e ${var2}`)
-console.log(`Resultado: ${resultadoMultiplicacao} entre ${var1} e ${var2}`)
-console.log(`Resultado: ${resultadoDivisao} entre ${var1} e ${var2}`)
+console.log(`Resultado da soma entre ${var1} e ${var2} é ${resultadoSoma}`)
+console.log(
+    `Resultado da subtração entre ${var1} e ${var2} é ${resultadoSubtracao}`
+)
+console.log(
+    `Resultado da multiplicação entre ${var1} e ${var2} é ${resultadoMultiplicacao}`
+)
+console.log(
+    `Resultado da divisão entre ${var1} e ${var2} é ${resultadoDivisao}`
+)
