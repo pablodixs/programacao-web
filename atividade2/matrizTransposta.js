8412b4084e55c9978fac582ad619b6a5cf329b80
Pablo <pabllosoarez@gmail.com>
2025-03-15 16:36:03 -0300

    add matrix transposition function

---
+++ atividade2/matrizTransposta.js
@@ -0,0 +1,25 @@
+function transporMatriz(A) {
+    const linhas = A.length
+    const colunas = A[0].length
+
+    const transp = []
+
+    for (let j = 0; j < colunas; j++) {
+        const novaLinha = []
+        for (let i = 0; i < linhas; i++) {
+            novaLinha.push(A[i][j])
+        }
+        transp.push(novaLinha)
+    }
+
+    console.log(A)
+    console.log(transp)
+}
+
+const A = [
+    [1, 2],
+    [3, 4],
+    [5, 6],
+]
+
+transporMatriz(A)
